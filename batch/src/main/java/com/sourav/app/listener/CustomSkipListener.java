658ed5930ee44b0d828f30b58993db796759933e
coder <abc@de.com>
2023-08-22 20:32:50 +0530

    add retry logic

--- batch/src/main/java/com/sourav/app/listener/CustomSkipListener.java
+++ batch/src/main/java/com/sourav/app/listener/CustomSkipListener.java
@@ -1,9 +1,9 @@
 package com.sourav.app.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sourav.app.json.model.Student;
 import org.springframework.batch.core.annotation.OnSkipInProcess;
 import org.springframework.batch.core.annotation.OnSkipInRead;
-import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +11,12 @@ import java.io.File;
 import java.io.FileWriter;
 
 @Component
-public class SkipListener {
+public class CustomSkipListener {
     @OnSkipInRead
     public void skipInRead(Throwable th) {
         if (th instanceof FlatFileParseException) {
             createFile("First Job/First Step/reader/CSVlogs.txt", ((FlatFileParseException) th).getInput());
-        } if (th instanceof ParseException) {
+        } if (th instanceof JsonProcessingException) {
             createFile("First Job/First Step/reader/JSONlogs.txt", th.getMessage());
         }
     }
